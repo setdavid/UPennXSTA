cbde607a5efe1891527e237dfec3fab34014cdaf
setdavid <setdavid1014@gmail.com>
2020-12-28 13:26:12 -0500

    restore to spa v2

--- js/spa-router-v2.js
+++ js/spa-router-v2.js
@@ -1,109 +1,109 @@
-// document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
-//     var hashLocationOnReady = window.location.hash;
-//     console.log("onready: " + hashLocationOnReady.length);
-//     if (hashLocationOnReady.length === 0) {
-//         window.location.hash = "#/home";
-//     } else {
-//         var pageOnReady = hashToPage(window.location.hash);
-//         navTo(pageOnReady);
-//     }
+    var hashLocationOnReady = window.location.hash;
+    console.log("onready: " + hashLocationOnReady.length);
+    if (hashLocationOnReady.length === 0) {
+        window.location.hash = "#/home";
+    } else {
+        var pageOnReady = hashToPage(window.location.hash);
+        navTo(pageOnReady);
+    }
 
-//     window.addEventListener("hashchange", function () {
-//         var page = hashToPage(window.location.hash);
-//         console.log("hashchange");
-//         navTo(page);
-//     });
+    window.addEventListener("hashchange", function () {
+        var page = hashToPage(window.location.hash);
+        console.log("hashchange");
+        navTo(page);
+    });
 
-//     function hashToPage(hashLocation) {
-//         var page = (hashLocation.slice(2)).replace(/[/]/g, "-");
-//         console.log("-----------HashLocation: " + hashLocation);
-//         console.log("Page: " + page);
-//         return page;
-//     }
+    function hashToPage(hashLocation) {
+        var page = (hashLocation.slice(2)).replace(/[/]/g, "-");
+        console.log("-----------HashLocation: " + hashLocation);
+        console.log("Page: " + page);
+        return page;
+    }
 
-//     function navTo(page) {
-//         jsUtils.closeSlider();
+    function navTo(page) {
+        jsUtils.closeSlider();
 
-//         console.log("NAVTO PAGE: " + page);
+        console.log("NAVTO PAGE: " + page);
 
-//         var requestUrlHTML = "pages/" + page + ".html";
-//         fetch(requestUrlHTML)
-//             .then((response) => {
-//                 if (response.ok) {
-//                     console.log("Status - HTML true: " + page);
-//                     return response.text();
-//                 } else {
-//                     console.log("Status - HTML failed: " + page);
-//                     return "navTo(error404)";
-//                 }
-//             }).then((text) => {
-//                 if (text != "navTo(error404)") {
-//                     document.querySelector("#main-content").innerHTML = text;
+        var requestUrlHTML = "pages/" + page + ".html";
+        fetch(requestUrlHTML)
+            .then((response) => {
+                if (response.ok) {
+                    console.log("Status - HTML true: " + page);
+                    return response.text();
+                } else {
+                    console.log("Status - HTML failed: " + page);
+                    return "navTo(error404)";
+                }
+            }).then((text) => {
+                if (text != "navTo(error404)") {
+                    document.querySelector("#main-content").innerHTML = text;
 
-//                     var requestUrlCSS = "css/null.css";
-//                     var requestUrlJS = "js/null.js";
+                    var requestUrlCSS = "css/null.css";
+                    var requestUrlJS = "js/null.js";
 
-//                     console.log("resultCSS: " + ((text.indexOf("<style>") != -1) && (text.indexOf("</style>") != -1)));
-//                     console.log("resultJS: " + ((text.indexOf("<script>") != -1) && (text.indexOf("</script>") != -1)));
+                    console.log("resultCSS: " + ((text.indexOf("<style>") != -1) && (text.indexOf("</style>") != -1)));
+                    console.log("resultJS: " + ((text.indexOf("<script>") != -1) && (text.indexOf("</script>") != -1)));
 
-//                     if ((text.indexOf("<style>") != -1) && (text.indexOf("</style>") != -1)) {
-//                         requestUrlCSS = "css/" + page + ".css";
-//                     }
+                    if ((text.indexOf("<style>") != -1) && (text.indexOf("</style>") != -1)) {
+                        requestUrlCSS = "css/" + page + ".css";
+                    }
 
-//                     if ((text.indexOf("<script>") != -1) && (text.indexOf("</script>") != -1)) {
-//                         requestUrlJS = "js/" + page + ".js";
-//                     }
+                    if ((text.indexOf("<script>") != -1) && (text.indexOf("</script>") != -1)) {
+                        requestUrlJS = "js/" + page + ".js";
+                    }
 
-//                     fetch(requestUrlCSS)
-//                         .then((response) => {
-//                             if (response.ok) {
-//                                 console.log("Status - CSS true: " + requestUrlCSS);
-//                                 cssChange(requestUrlCSS);
-//                             } else {
-//                                 console.log("Status - CSS failed: " + page);
-//                                 cssChange("css/null.css");
-//                             }
-//                         }).
-//                         catch((error) => { });
+                    fetch(requestUrlCSS)
+                        .then((response) => {
+                            if (response.ok) {
+                                console.log("Status - CSS true: " + requestUrlCSS);
+                                cssChange(requestUrlCSS);
+                            } else {
+                                console.log("Status - CSS failed: " + page);
+                                cssChange("css/null.css");
+                            }
+                        }).
+                        catch((error) => { });
 
-//                     fetch(requestUrlJS)
-//                         .then((response) => {
-//                             if (response.ok) {
-//                                 console.log("Status - JS true: " + requestUrlJS);
-//                                 scriptChange(requestUrlJS);
-//                             } else {
-//                                 console.log("Status - JS failed: " + page);
-//                                 scriptChange("js/null.js");
-//                             }
-//                         }).
-//                         catch((error) => { });
-//                 } else {
-//                     navTo("error404");
-//                 }
-//             }).
-//             catch((error) => {
-//                 // console.log("Status - CSS caught error: " + page);
-//                 // cssChange("css/error404.css");
-//                 // console.log("Status - JS caught error: " + error);
-//                 // scriptChange("js/null.js");
-//             });
+                    fetch(requestUrlJS)
+                        .then((response) => {
+                            if (response.ok) {
+                                console.log("Status - JS true: " + requestUrlJS);
+                                scriptChange(requestUrlJS);
+                            } else {
+                                console.log("Status - JS failed: " + page);
+                                scriptChange("js/null.js");
+                            }
+                        }).
+                        catch((error) => { });
+                } else {
+                    navTo("error404");
+                }
+            }).
+            catch((error) => {
+                // console.log("Status - CSS caught error: " + page);
+                // cssChange("css/error404.css");
+                // console.log("Status - JS caught error: " + error);
+                // scriptChange("js/null.js");
+            });
 
-//         window.scrollTo(0, 0);
-//     }
+        window.scrollTo(0, 0);
+    }
 
-//     function scriptChange(requestUrl) {
-//         jsUtils.refresh();
+    function scriptChange(requestUrl) {
+        jsUtils.refresh();
 
-//         // $("script[src*='page']").remove();
-//         $("body > script:nth-last-child(2)").remove();
-//         var newScript = document.createElement("script");
-//         newScript.src = requestUrl;
-//         document.body.appendChild(newScript);
-//     }
+        // $("script[src*='page']").remove();
+        $("body > script:nth-last-child(2)").remove();
+        var newScript = document.createElement("script");
+        newScript.src = requestUrl;
+        document.body.appendChild(newScript);
+    }
 
-//     function cssChange(requestUrl) {
-//         $("head > link:last").remove();
-//         $("head").append("<link rel='stylesheet' href='" + requestUrl + "'>");
-//     }
-// });
+    function cssChange(requestUrl) {
+        $("head > link:last").remove();
+        $("head").append("<link rel='stylesheet' href='" + requestUrl + "'>");
+    }
+});
--- js/spa-router-v3.js
+++ js/spa-router-v3.js
@@ -1,152 +1,152 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    let pathsJSON = null;
-    fetch("paths.json")
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return null;
-            }
-        }).then((json) => {
-            console.log(json != null);
-            if (json != null) {
-                pathsJSON = json;
+//     let pathsJSON = null;
+//     fetch("paths.json")
+//         .then((response) => {
+//             if (response.ok) {
+//                 return response.json();
+//             } else {
+//                 return null;
+//             }
+//         }).then((json) => {
+//             console.log(json != null);
+//             if (json != null) {
+//                 pathsJSON = json;
 
-                window.addEventListener("hashchange", function () {
-                    navTo(window.location.hash);
-                });
+//                 window.addEventListener("hashchange", function () {
+//                     navTo(window.location.hash);
+//                 });
 
-                var hashLocationOnReady = window.location.hash;
-                console.log("onready: " + hashLocationOnReady.length);
-                if (hashLocationOnReady.length === 0) {
-                    window.location.hash = "#/home";
-                } else {
-                    navTo(window.location.hash);
-                }
-            }
-        }).catch((error) => { });
+//                 var hashLocationOnReady = window.location.hash;
+//                 console.log("onready: " + hashLocationOnReady.length);
+//                 if (hashLocationOnReady.length === 0) {
+//                     window.location.hash = "#/home";
+//                 } else {
+//                     navTo(window.location.hash);
+//                 }
+//             }
+//         }).catch((error) => { });
 
-    let pageToPath = function (page) {
-        return page.slice(2);
-    }
+//     let pageToPath = function (page) {
+//         return page.slice(2);
+//     }
 
-    let pathToArr = function (path) {
-        return path.split("/");
-    }
+//     let pathToArr = function (path) {
+//         return path.split("/");
+//     }
 
-    let pathArrToTarget = function (pathArr) {
-        // console.log("-----------HashLocation: " + hashLocation);
-        // console.log("Page: " + page);
+//     let pathArrToTarget = function (pathArr) {
+//         // console.log("-----------HashLocation: " + hashLocation);
+//         // console.log("Page: " + page);
 
-        let pathTarget = null;
+//         let pathTarget = null;
 
-        if (pathArr[0] in pathsJSON) {
-            pathTarget = pathsJSON[pathArr[0]];
+//         if (pathArr[0] in pathsJSON) {
+//             pathTarget = pathsJSON[pathArr[0]];
 
-            for (let i = 1; i < pathArr.length && pathTarget != null; i++) {
-                if (pathArr[i] in pathTarget) {
-                    pathTarget = pathTarget[pathArr[i]];
-                } else {
-                    pathTarget = null;
-                }
-            }
-        }
+//             for (let i = 1; i < pathArr.length && pathTarget != null; i++) {
+//                 if (pathArr[i] in pathTarget) {
+//                     pathTarget = pathTarget[pathArr[i]];
+//                 } else {
+//                     pathTarget = null;
+//                 }
+//             }
+//         }
 
-        return pathTarget;
-    }
+//         return pathTarget;
+//     }
 
-    function navTo(page) {
-        let path = pageToPath(page);
-        let target = pathArrToTarget(pathToArr(path));
+//     function navTo(page) {
+//         let path = pageToPath(page);
+//         let target = pathArrToTarget(pathToArr(path));
 
-        if (target != null) {
-            jsUtils.closeSlider();
+//         if (target != null) {
+//             jsUtils.closeSlider();
 
-            console.log("NAVTO PAGE: " + path);
+//             console.log("NAVTO PAGE: " + path);
 
-            fetch("pages/" + path + ".html")
-                .then((response) => {
-                    if (response.ok) {
-                        console.log("Status - HTML true: " + path);
-                        return response.text();
-                    } else {
-                        console.log("Status - HTML failed: " + path);
-                        return "navTo(#/error404)";
-                    }
-                }).then((text) => {
-                    if (text != "navTo(#/error404)") {
-                        document.querySelector("#main-content").innerHTML = text;
+//             fetch("pages/" + path + ".html")
+//                 .then((response) => {
+//                     if (response.ok) {
+//                         console.log("Status - HTML true: " + path);
+//                         return response.text();
+//                     } else {
+//                         console.log("Status - HTML failed: " + path);
+//                         return "navTo(#/error404)";
+//                     }
+//                 }).then((text) => {
+//                     if (text != "navTo(#/error404)") {
+//                         document.querySelector("#main-content").innerHTML = text;
 
-                        var requestUrlCSS = "css/null.css";
-                        var requestUrlJS = "js/null.js";
+//                         var requestUrlCSS = "css/null.css";
+//                         var requestUrlJS = "js/null.js";
 
-                        // console.log("resultCSS: " + ((text.indexOf("<style>") != -1) && (text.indexOf("</style>") != -1)));
-                        // console.log("resultJS: " + ((text.indexOf("<script>") != -1) && (text.indexOf("</script>") != -1)));
+//                         // console.log("resultCSS: " + ((text.indexOf("<style>") != -1) && (text.indexOf("</style>") != -1)));
+//                         // console.log("resultJS: " + ((text.indexOf("<script>") != -1) && (text.indexOf("</script>") != -1)));
 
-                        console.log("CSS: " + target["CSS"]);
-                        console.log("JS: " + target["JS"]);
+//                         console.log("CSS: " + target["CSS"]);
+//                         console.log("JS: " + target["JS"]);
 
-                        if (target["CSS"]) {
-                            requestUrlCSS = "css/" + path + ".css";
-                        }
+//                         if (target["CSS"]) {
+//                             requestUrlCSS = "css/" + path + ".css";
+//                         }
 
-                        if (target["JS"]) {
-                            requestUrlJS = "js/" + path + ".js";
-                        }
+//                         if (target["JS"]) {
+//                             requestUrlJS = "js/" + path + ".js";
+//                         }
 
-                        fetch(requestUrlCSS)
-                            .then((response) => {
-                                if (response.ok) {
-                                    console.log("Status - CSS true: " + requestUrlCSS);
-                                    cssChange(requestUrlCSS);
-                                } else {
-                                    console.log("Status - CSS failed: " + page);
-                                    cssChange("css/null.css");
-                                }
-                            }).
-                            catch((error) => { });
+//                         fetch(requestUrlCSS)
+//                             .then((response) => {
+//                                 if (response.ok) {
+//                                     console.log("Status - CSS true: " + requestUrlCSS);
+//                                     cssChange(requestUrlCSS);
+//                                 } else {
+//                                     console.log("Status - CSS failed: " + page);
+//                                     cssChange("css/null.css");
+//                                 }
+//                             }).
+//                             catch((error) => { });
 
-                        fetch(requestUrlJS)
-                            .then((response) => {
-                                if (response.ok) {
-                                    console.log("Status - JS true: " + requestUrlJS);
-                                    scriptChange(requestUrlJS);
-                                } else {
-                                    console.log("Status - JS failed: " + page);
-                                    scriptChange("js/null.js");
-                                }
-                            }).
-                            catch((error) => { });
-                    } else {
-                        navTo("#/error404");
-                    }
-                }).
-                catch((error) => {
-                    // console.log("Status - CSS caught error: " + page);
-                    // cssChange("css/#/error404.css");
-                    // console.log("Status - JS caught error: " + error);
-                    // scriptChange("js/null.js");
-                });
+//                         fetch(requestUrlJS)
+//                             .then((response) => {
+//                                 if (response.ok) {
+//                                     console.log("Status - JS true: " + requestUrlJS);
+//                                     scriptChange(requestUrlJS);
+//                                 } else {
+//                                     console.log("Status - JS failed: " + page);
+//                                     scriptChange("js/null.js");
+//                                 }
+//                             }).
+//                             catch((error) => { });
+//                     } else {
+//                         navTo("#/error404");
+//                     }
+//                 }).
+//                 catch((error) => {
+//                     // console.log("Status - CSS caught error: " + page);
+//                     // cssChange("css/#/error404.css");
+//                     // console.log("Status - JS caught error: " + error);
+//                     // scriptChange("js/null.js");
+//                 });
 
-            window.scrollTo(0, 0);
-        } else {
-            navTo("#/error404");
-        }
-    }
+//             window.scrollTo(0, 0);
+//         } else {
+//             navTo("#/error404");
+//         }
+//     }
 
-    function scriptChange(requestUrl) {
-        jsUtils.refresh();
+//     function scriptChange(requestUrl) {
+//         jsUtils.refresh();
 
-        // $("script[src*='page']").remove();
-        $("body > script:nth-last-child(2)").remove();
-        var newScript = document.createElement("script");
-        newScript.src = requestUrl;
-        document.body.appendChild(newScript);
-    }
+//         // $("script[src*='page']").remove();
+//         $("body > script:nth-last-child(2)").remove();
+//         var newScript = document.createElement("script");
+//         newScript.src = requestUrl;
+//         document.body.appendChild(newScript);
+//     }
 
-    function cssChange(requestUrl) {
-        $("head > link:last").remove();
-        $("head").append("<link rel='stylesheet' href='" + requestUrl + "'>");
-    }
-});
+//     function cssChange(requestUrl) {
+//         $("head > link:last").remove();
+//         $("head").append("<link rel='stylesheet' href='" + requestUrl + "'>");
+//     }
+// });
